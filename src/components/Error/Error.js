5b338fb59b62c893f4e10634d7069c8a1a6e8fff
Mustafa Emir <info@mustafaemir.com>
2022-01-21 01:16:49 +0300

    Create error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Error({message}) {
+  return (
+    <>
+      <span className="relative inline-flex">
+        <button type="button" className="flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-sky-500 bg-white dark:bg-slate-800 transition ease-in-out duration-150 cursor-not-allowed ring-1 ring-slate-900/10 dark:ring-slate-200/20" disabled="">
+          {message}
+        </button>
+        <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+        </span>
+      </span>
+    </>
+  );
+}
+
+export default Error;
