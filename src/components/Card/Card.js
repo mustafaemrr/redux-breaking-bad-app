05760200640beb29ec166b20b4c71fa93109fb57
Mustafa Emir <info@mustafaemir.com>
2022-01-22 14:51:17 +0300

    Refactor Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import './Card.scss';
 
-function Card({image, title}) {
+function Card({image, title, id}) {
   return (
     <div className="card break-inside">
-      <img className="card__image" src={image} alt={title} />
-      <div className="card__title">
-        {title}
-      </div>
+      <Link to={`/char/${id}`}>
+        <img className="card__image" src={image} alt={title} />
+        <div className="card__title">
+          {title}
+        </div>
+      </Link>
     </div>
   );
 }
